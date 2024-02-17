2981baded617ee7a384f2be88223fcf968b5e648
akilaramesh1910 <130809341+akilaramesh1910@users.noreply.github.com>
2024-02-17 20:39:16 +0530

    new one

---
+++ BuyandSell.java
@@ -0,0 +1,18 @@
+public class BuyandSell {
+    public static void main(String[] args) {
+        int arr[]={7,1,5,4,6,3};
+        int value=maxProfit(arr);
+        System.out.println(value);
+    }
+    public static int maxProfit(int[] prices) {
+        int profit=0;
+        int cost_price=prices[0];
+        for(int i=1;i<prices.length;i++){
+           if(cost_price>=prices[i]){
+              cost_price=prices[i];
+           }
+           profit=Math.max(profit,prices[i]-cost_price);
+        }
+        return profit;
+    }
+}
